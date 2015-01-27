a69248e402edc644cf01549833509330b67353c0
Alexander Barabash <alexander_barabash@mentor.com>
2015-01-27 15:22:16 +0200

    Added bscript_value accessors.

--- include/qemu/bscript_value.h
+++ include/qemu/bscript_value.h
@@ -124,5 +124,172 @@ static inline uint32_t bscript_value_get_flag(struct bscript_value *s)
 bool bscript_value_write(struct bscript_value *s);
 bool bscript_value_read(struct bscript_value *s);
 
+static inline bool bscript_value_write8(struct bscript_value *s, uint8_t v)
+{
+    bscript_value_set8(s, v);
+    return bscript_value_write(s);
+}
+
+static inline bool bscript_value_write16(struct bscript_value *s, uint16_t v)
+{
+    bscript_value_set16(s, v);
+    return bscript_value_write(s);
+}
+
+static inline bool bscript_value_write32(struct bscript_value *s, uint32_t v)
+{
+    bscript_value_set32(s, v);
+    return bscript_value_write(s);
+}
+
+static inline bool bscript_value_write64(struct bscript_value *s, uint64_t v)
+{
+    bscript_value_set64(s, v);
+    return bscript_value_write(s);
+}
+
+static inline bool bscript_value_write32_flag(struct bscript_value *s,
+                                              uint32_t v, uint32_t flag)
+{
+    bscript_value_set32(s, v);
+    bscript_value_set_flag(s, flag);
+    return bscript_value_write(s);
+}
+
+static inline bool bscript_value_write64_flag(struct bscript_value *s,
+                                              uint64_t v, uint32_t flag)
+{
+    bscript_value_set64(s, v);
+    bscript_value_set_flag(s, flag);
+    return bscript_value_write(s);
+}
+
+static inline bool bscript_value_read8(struct bscript_value *s, int8_t *v)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get8(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_read16(struct bscript_value *s, int16_t *v)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get16(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_read32(struct bscript_value *s, int32_t *v)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get32(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_read64(struct bscript_value *s, int64_t *v)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get64(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_readu8(struct bscript_value *s, uint8_t *v)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get8(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_readu16(struct bscript_value *s, uint16_t *v)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get16(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_readu32(struct bscript_value *s, uint32_t *v)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get32(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_readu64(struct bscript_value *s, uint64_t *v)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get64(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_read32_flag(struct bscript_value *s,
+                                             int32_t *v, uint32_t *flag)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get32(s);
+        *flag = bscript_value_get_flag(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_read64_flag(struct bscript_value *s,
+                                             int64_t *v, uint32_t *flag)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get64(s);
+        *flag = bscript_value_get_flag(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_readu32_flag(struct bscript_value *s,
+                                              uint32_t *v, uint32_t *flag)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get32(s);
+        *flag = bscript_value_get_flag(s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static inline bool bscript_value_readu64_flag(struct bscript_value *s,
+                                              uint64_t *v, uint32_t *flag)
+{
+    if (bscript_value_read(s)) {
+        *v = bscript_value_get64(s);
+        *flag = bscript_value_get_flag(s);
+        return true;
+    } else {
+        return false;
+    }
+}
 
 #endif
