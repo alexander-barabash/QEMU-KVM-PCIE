cd37c11a38a293b29153dfe9fff3762828696e4e
Alexander Barabash <alexander_barabash@mentor.com>
2015-03-18 19:43:24 +0200

    Don't _FORTIFY_SOURCE for tpm_passthrough.

--- hw/tpm/tpm_passthrough.c
+++ hw/tpm/tpm_passthrough.c
@@ -22,6 +22,10 @@
  * License along with this library; if not, see <http://www.gnu.org/licenses/>
  */
 
+#ifdef _FORTIFY_SOURCE
+#undef _FORTIFY_SOURCE
+#endif
+
 #include <dirent.h>
 
 #include "qemu-common.h"
