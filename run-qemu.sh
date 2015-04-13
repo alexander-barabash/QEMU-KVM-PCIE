cf7768e9f848a4b4393502f6c79f0139baa78ae2
Alexander Barabash <alexander_barabash@mentor.com>
2015-04-13 13:06:47 +0300

    Support using run-qemu-system-* with /usr/bin/env shebang (using -readconfig).

--- run-qemu.sh
+++ run-qemu.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 NAME=`basename $0`
 NAME=`echo $NAME | sed s/^run-//`
+
+READCONFIG=
+if [ ! -z "$1" ]
+then
+    if [ -f "$1" ]
+    then
+        READCONFIG="-readconfig $1"
+        shift
+    fi
+fi
+
 export LD_LIBRARY_PATH=
-exec `dirname $0`/bin/$NAME "$@"
+echo `dirname $0`/bin/$NAME $READCONFIG "$@"
+exec `dirname $0`/bin/$NAME $READCONFIG "$@"
